aae224677dbacc20e3c9cc0401246aeb428c2ecb
OrichalcosShunnoros <c3146092091@gmail.com>
2025-05-16 09:35:24 -0500

    backend iniciado y configurado

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.send('Backend with TypeScript ready!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
